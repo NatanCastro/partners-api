e6fe955e0f03aac25709dfa12002c956cf5c07de
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-10-16 02:53:28 -0300

    chore: create project

---
+++ src/main/java/dev/natan/ze/challenge/ZeChallengeApplication.java
@@ -0,0 +1,13 @@
+package dev.natan.ze.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/natan/ze/challenge/ZeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.natan.ze.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
