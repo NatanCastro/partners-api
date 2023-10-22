cae34d1c6b9b6bb63e78e4b318f6a3b43572c265
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-10-22 06:02:00 -0300

    feat: create rest controller for partner entity
    Signed-off-by: Natan Gabriel Almeida de Castro <castro.natan@outlook.com>

---
+++ src/main/java/dev/natan/ze/challenge/application/rest/PartnerController.java
@@ -0,0 +1,35 @@
+package dev.natan.ze.challenge.application.rest;
+
+import dev.natan.ze.challenge.application.usecase.PartnerUsecase;
+import dev.natan.ze.challenge.domain.dto.CreatePartnerDto;
+import dev.natan.ze.challenge.domain.dto.FindPartnerDto;
+import dev.natan.ze.challenge.domain.entity.Partner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/partners", produces = "application/json")
+public class PartnerController {
+  @Autowired
+  private PartnerUsecase partnerUsecase;
+
+  @GetMapping()
+  public List<Partner> findAll(@RequestParam("lat") Double lat, @RequestParam("lng") Double lng) {
+    FindPartnerDto findPartnerDto = new FindPartnerDto(lat, lng);
+    return partnerUsecase.getPartners(findPartnerDto);
+  }
+
+  @GetMapping(value = "/{id}", produces = "application/json")
+  @ResponseStatus(value = org.springframework.http.HttpStatus.OK)
+  public Partner findById(@PathVariable("id") Long id) {
+    return partnerUsecase.getPartner(id);
+  }
+
+  @PostMapping(value = "populate", consumes = "application/json")
+  @ResponseStatus(value = org.springframework.http.HttpStatus.CREATED)
+  public void populate(@RequestBody List<CreatePartnerDto> data) {
+    partnerUsecase.populate(data);
+  }
+}
