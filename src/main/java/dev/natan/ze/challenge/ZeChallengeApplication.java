b58a16c94f731d3a6ec51ef2188bcf9e546101d2
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-10-22 05:50:29 -0300

    feat: enable mongo repositories
    Signed-off-by: Natan Gabriel Almeida de Castro <castro.natan@outlook.com>

--- src/main/java/dev/natan/ze/challenge/ZeChallengeApplication.java
+++ src/main/java/dev/natan/ze/challenge/ZeChallengeApplication.java
@@ -2,8 +2,10 @@ package dev.natan.ze.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "dev.natan.ze.challenge.infrastructure.db")
 public class ZeChallengeApplication {
 
 	public static void main(String[] args) {
