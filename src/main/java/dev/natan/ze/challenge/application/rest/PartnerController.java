2527a1103ff4e11a2171c7cb648db201c9f510ec
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2023-10-22 06:50:36 -0300

    feat: create endpoint for updating partner
    Signed-off-by: Natan Gabriel Almeida de Castro <castro.natan@outlook.com>

--- src/main/java/dev/natan/ze/challenge/application/rest/PartnerController.java
+++ src/main/java/dev/natan/ze/challenge/application/rest/PartnerController.java
@@ -3,8 +3,10 @@ package dev.natan.ze.challenge.application.rest;
 import dev.natan.ze.challenge.application.usecase.PartnerUsecase;
 import dev.natan.ze.challenge.domain.dto.CreatePartnerDto;
 import dev.natan.ze.challenge.domain.dto.FindPartnerDto;
+import dev.natan.ze.challenge.domain.dto.UpdatePartnerDto;
 import dev.natan.ze.challenge.domain.entity.Partner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,20 +23,26 @@ public class PartnerController {
     return partnerUsecase.getPartners(findPartnerDto);
   }
 
-  @GetMapping(value = "/{id}", produces = "application/json")
-  @ResponseStatus(value = org.springframework.http.HttpStatus.OK)
+  @GetMapping(value = "/{id}")
+  @ResponseStatus(value = HttpStatus.OK)
   public Partner findById(@PathVariable("id") Long id) {
     return partnerUsecase.getPartner(id);
   }
 
-  @PostMapping(consumes = "application/json", produces = "application/json")
-  @ResponseStatus(value = org.springframework.http.HttpStatus.CREATED)
+  @PatchMapping(value = "/{id}", consumes = "application/json")
+  @ResponseStatus(value = HttpStatus.OK)
+  public Partner update(@PathVariable("id") Long id, @RequestBody UpdatePartnerDto partner) {
+    return partnerUsecase.updatePartner(id, partner);
+  }
+
+  @PostMapping(consumes = "application/json")
+  @ResponseStatus(value = HttpStatus.CREATED)
   public Partner create(@RequestBody CreatePartnerDto data) {
     return partnerUsecase.insertPartner(data);
   }
 
   @PostMapping(value = "populate", consumes = "application/json")
-  @ResponseStatus(value = org.springframework.http.HttpStatus.CREATED)
+  @ResponseStatus(value = HttpStatus.CREATED)
   public void populate(@RequestBody List<CreatePartnerDto> data) {
     partnerUsecase.populate(data);
   }
